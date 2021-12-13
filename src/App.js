ba01ea479a5e5313c2da1aadbfe6c8d898258f1d
Xavier <montjuic79@gmail.com>
2021-12-13 09:41:08 +0100

    Router basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//Components:
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import NewPost from "./pages/NewPost";
+import EditPost from "./pages/EditPost";
+import PostPage from "./pages/PostPage";
+import About from "./pages/About";
+import Missing from "./pages/Missing";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/post">
+          <NewPost />
+        </Route>
+        <Route exact path="/edit">
+          <EditPost />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <Missing />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Missing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Missing = () => {
+  return <div>Page not Found</div>;
+};
+
+export default Missing;
