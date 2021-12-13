f722d31cb26c3c02a0f336f625e261725c7f27d9
Xavier <montjuic79@gmail.com>
2021-12-13 17:51:11 +0100

    Fetch posts with custom hook useAxiosFetch

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Missing from "./pages/Missing";
 import { Switch, Route, useHistory } from "react-router-dom";
 import { format } from "date-fns";
 import api from "./api/posts";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -24,24 +25,31 @@ function App() {
   const [editBody, setEditBody] = useState("");
 
   const history = useHistory();
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts"
+  );
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get("/posts");
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
-    fetchPosts();
-  }, []);
+    setPosts(data);
+  }, [data]);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await api.get("/posts");
+  //       setPosts(response.data);
+  //     } catch (err) {
+  //       if (err.response) {
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   };
+  //   fetchPosts();
+  // }, []);
 
   const handleEdit = async (id) => {
     const datetime = format(new Date(), "MMM dd, yyyy pp");
@@ -114,7 +122,11 @@ function App() {
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
-          <Home posts={searchResults} />
+          <Home
+            posts={searchResults}
+            fetchError={fetchError}
+            isLoading={isLoading}
+          />
         </Route>
         <Route exact path="/post">
           <NewPost
